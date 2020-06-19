726039ded460d9c3274068c8cd2d7759a07e74db
KhanhTM <44893766+gawe0405@users.noreply.github.com>
2020-06-19 22:16:41 +0700

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$localhost="127.0.0.1";
-$username="root";
-$passwword="";
-$dbname="mobile";
-$conn=mysqli_connect($localhost, $username, $passwword, $dbname) or die('Xin lỗi, database không kết nối được.');
-?>
